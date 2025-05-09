c8340fe8215ebfd49bbdb8ab930aea7d296dff86
vityayakymchuk <147533752+vityayakymchuk@users.noreply.github.com>
2025-05-09 23:59:24 +0200

    prodam dushu

---
+++ src/js/taro-swiper.js
@@ -0,0 +1,22 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.taro-swiper-init', {
+  loop: true,
+  navigation: {
+    nextEl: '.taro-right-swiper-btn',
+    prevEl: '.taro-left-swiper-btn',
+  },
+  allowTouchMove: true,
+  autoHeight: true,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  centeredSlides: true,
+});
+
+document.querySelectorAll('.taro-left-swiper-btn, .taro-right-swiper-btn')
+  .forEach(btn => {
+    btn.addEventListener('click', () => {
+      btn.blur();
+    });
+  });
