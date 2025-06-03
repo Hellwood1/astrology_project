8778acb515600b56c48650f8ff2c784477856558
Hellwood1 <samburik123@gmail.com>
2025-06-03 23:28:39 +0200

    Upd.  ''gift meditation'' section
    - Переписав логіку для шпалер.

--- src/js/gift-over.js
+++ src/js/gift-over.js
@@ -1,12 +1,23 @@
 const facade = document.querySelector('.meditation-video-facade');
+
 facade.addEventListener('click', () => {
   const videoId = facade.dataset.videoId;
+
   const iframe = document.createElement('iframe');
-  iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&showinfo=0`;
+  iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&showinfo=0`; //
+
   iframe.setAttribute('frameborder', '0');
   iframe.setAttribute('allow', 'autoplay; encrypted-media');
   iframe.setAttribute('allowfullscreen', '');
-  iframe.className = 'meditation-video';
 
-  facade.replaceWith(iframe);
+  // Стилізація
+  iframe.style.position = 'absolute';
+  iframe.style.inset = '0';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
+  iframe.style.zIndex = '3';
+
+  facade.innerHTML = '';
+  facade.appendChild(iframe);
 });
