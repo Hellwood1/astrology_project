3cb4d6dad66602d40c30e4963b0e2668b283fb30
Hellwood1 <samburik123@gmail.com>
2025-06-09 23:11:45 +0200

    Add. Fix build base path for production

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: './',
+    base: '/',
     build: {
       sourcemap: true,
       rollupOptions: {
